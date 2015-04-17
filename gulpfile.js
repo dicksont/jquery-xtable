76025f909ea14648f9d5802b3930a0debd5d773c
Dickson Tam <dickson.tam@icloud.com>
2015-04-17 15:49:20 -0700

    Bumped version to 0.2.0. Simplified file layout. Refactored module layout.

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,10 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-//var fs = require('fs');
-//var extend = require('extend');
-var basename = require('path').basename;
-//var resolve = require('path').resolve;
-var dir = require('path').dirname;
-var strip = require('gulp-strip-comments');
-var clean = require('gulp-clean');
 var bump = require('gulp-bump');
-var evstr = require('event-stream');
-var symlink = require('gulp-symlink');
-
-var srcPath = {}, buildPath = {}, distPath = {}, buildVersion = {}, meta = {};
-
-
-meta.package = "package.json";
-meta.bower = 'bower.json';
-
-srcPath.tablejs = 'src/table.js';
-srcPath.quadjs = 'src/quad.js';
-srcPath.css = 'src/table.css';
-srcPath.license = 'LICENSE';
-
-buildPath.js = 'build/jquery-xtable.js';
-buildPath.css = 'build/jquery-xtable.css';
-
-distPath.dir = 'dist/';
-distPath.js = 'dist/jquery-xtable.js';
-distPath.css = 'dist/jquery-xtable.css';
-
-
-gulp.task('buildjs', function() {
-  return evstr.merge(gulp.src(srcPath.quadjs), gulp.src(srcPath.tablejs).pipe(strip()))
-      .pipe(concat(basename(buildPath.js)))
-      .pipe(gulp.dest(dir(buildPath.js)));
-});
-
-gulp.task('css', function() {
-  return gulp.src(srcPath.css)
-    .pipe(concat(basename(buildPath.css)))
-    .pipe(gulp.dest(dir(buildPath.css)));
-})
 
 gulp.task('bump', function() {
-  var stream = gulp.src([ meta.bower, meta.package ])
+  var stream = gulp.src([ "package.json", "bower.json" ])
     .pipe(bump({type: 'patch'}))
     .pipe(gulp.dest('./'));
 
   return stream;
-});
-
-gulp.task('distpatch', ['buildjs', 'css'], function() {
-  return gulp.src([buildPath.js, buildPath.css])
-    .pipe(gulp.dest(distPath.dir));
-});
-
-gulp.task('mkpatch', ['distpatch']);
-
-gulp.task('clean', function() {
-  return gulp.src([ distPath.dir, dir(buildPath.js), dir(buildPath.css) ])
-    .pipe(clean());
-})
-
-gulp.task('watch', function() {
-  gulp.watch('src/**', ['mkpatch']);
 });
--- spec/table_spec.js
+++ spec/table_spec.js
@@ -1,39 +1,39 @@
 QUnit.test( ".base", function( assert ) {
-  assert.ok($.table('table.buster').base('A2').column('B').text() == $.table('table.buster').base('B2').column('A').text(), "base('A2').column('B') == base('B2').column('A')");
-  assert.ok($.table('table.buster').base('A1').row(2).text() == $.table('table.buster').base('A2').row(1).text(), "base('A1').row(2) == base('A2').row(1)");
+  assert.ok(jQuery.table('table.buster').base('A2').column('B').text() == jQuery.table('table.buster').base('B2').column('A').text(), "base('A2').column('B') == base('B2').column('A')");
+  assert.ok(jQuery.table('table.buster').base('A1').row(2).text() == jQuery.table('table.buster').base('A2').row(1).text(), "base('A1').row(2) == base('A2').row(1)");
 });
 
 QUnit.test( ".text", function( assert ) {
-  assert.ok($.table('table.buster').base('A2').column('A').text() == "24 25 26 27", "column('A').text() == '24 25 26 27'");
-  assert.ok($.table('table.buster').base('A2').row(1).text() == "24 45 .284 4", "row(1).text() == '24 45 .284 4'");
-  assert.ok($.table('table.buster').base('A2').cell('A1').text() == "24", "cell('A1').text() == '24'");
+  assert.ok(jQuery.table('table.buster').base('A2').column('A').text() == "24 25 26 27", "column('A').text() == '24 25 26 27'");
+  assert.ok(jQuery.table('table.buster').base('A2').row(1).text() == "24 45 .284 4", "row(1).text() == '24 45 .284 4'");
+  assert.ok(jQuery.table('table.buster').base('A2').cell('A1').text() == "24", "cell('A1').text() == '24'");
 });
 
 QUnit.test( ".value", function( assert ) {
-  assert.ok($.table('table.buster').base('A2').cell('A1').value() == 24, "cell('A1').value() == 24");
+  assert.ok(jQuery.table('table.buster').base('A2').cell('A1').value() == 24, "cell('A1').value() == 24");
 });
 
 QUnit.test(".sum", function(assert) {
-  assert.ok($.table('table.buster').base('A2').column('D').sum() == 65, "column('D').sum() == 65");
-  assert.ok($.table('table.buster').base('A2').range('D1:D4').sum() == 65, "range('D1:D4').sum() == 65");
-  assert.ok($.table('table.buster').base('A2').range('D2:D4').sum() == 61, "range('D2:D4').sum() == 61");
+  assert.ok(jQuery.table('table.buster').base('A2').column('D').sum() == 65, "column('D').sum() == 65");
+  assert.ok(jQuery.table('table.buster').base('A2').range('D1:D4').sum() == 65, "range('D1:D4').sum() == 65");
+  assert.ok(jQuery.table('table.buster').base('A2').range('D2:D4').sum() == 61, "range('D2:D4').sum() == 61");
 });
 
 QUnit.test(".max", function(assert) {
-  assert.ok($.table('table.buster').base('A2').column('D').max() == 24, "column('D').max() == 24");
-  assert.ok($.table('table.buster').base('A2').range('D2:D4').max() == 24, "column('D').range('D1:D4').max() == 24");
-  assert.ok($.table('table.buster').base('A2').range('D2:D4').sum() == 61, "column('D').range('D2:D4').sum() == 61");
+  assert.ok(jQuery.table('table.buster').base('A2').column('D').max() == 24, "column('D').max() == 24");
+  assert.ok(jQuery.table('table.buster').base('A2').range('D2:D4').max() == 24, "column('D').range('D1:D4').max() == 24");
+  assert.ok(jQuery.table('table.buster').base('A2').range('D2:D4').sum() == 61, "column('D').range('D2:D4').sum() == 61");
 });
 
 QUnit.test(".select", function(assert) {
-  assert.ok($.table('table.buster').column('A').select().text() == $.table('table.buster').column('A').text(), "column('A').select() == column('A')");
-  assert.ok($.table('table.buster').range('D1:D3').select().text() == $.table('table.buster').range('D1:D3').text(), "range('D1:D3').select() == range('D1:D3')");
+  assert.ok(jQuery.table('table.buster').column('A').select().text() == jQuery.table('table.buster').column('A').text(), "column('A').select() == column('A')");
+  assert.ok(jQuery.table('table.buster').range('D1:D3').select().text() == jQuery.table('table.buster').range('D1:D3').text(), "range('D1:D3').select() == range('D1:D3')");
 });
 
 QUnit.test(".all", function(assert) {
-  assert.ok($.table.all().length == $('table').length, "all().length == $('table').length");
-  assert.ok($.table.all()[0].get(0) == $('table')[0], "all()[0] == $('table')[0]");
-  assert.ok($.table.all()[1].get(0) == $('table')[1], "all()[1] == $('table')[1]");
+  assert.ok(jQuery.table.all().length == jQuery('table').length, "all().length == $('table').length");
+  assert.ok(jQuery.table.all()[0].get(0) == jQuery('table')[0], "all()[0] == $('table')[0]");
+  assert.ok(jQuery.table.all()[1].get(0) == jQuery('table')[1], "all()[1] == $('table')[1]");
 });
 
 QUnit.load();
